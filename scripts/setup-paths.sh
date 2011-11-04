7d8366a27cfe0d839233f1a215e4420b662d88f5
Mathieu Deletrain <mathieudel@gmail.com>
2011-11-04 04:18:06 +0100

    First pass at fixing build scripts.

--- scripts/setup-paths.sh
+++ scripts/setup-paths.sh
@@ -6,6 +6,6 @@ source utils.sh
 ALEPH_DIR=AlephOne
 APPS_DIR=apps
 
-SOURCES_PATH=$(getAbsolutePath aleph-one)
-BUILD_PATH=$(getAbsolutePath build)
-BIN_PATH=$(getAbsolutePath bin)
+SOURCES_PATH=$(getAbsolutePath ../aleph-one)
+BUILD_PATH=$(getAbsolutePath ../build)
+BIN_PATH=$(getAbsolutePath ../bin)
