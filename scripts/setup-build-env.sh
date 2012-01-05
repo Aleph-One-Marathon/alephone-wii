e697b38c7928c23ab53dc2feadf7df0341642aae
Mathieu Deletrain <mathieudel@gmail.com>
2012-01-05 23:10:34 +0100

    Enabled O3 optimizations by default. Removed some useless (I hope) link and preprocessor flags.

--- scripts/setup-build-env.sh
+++ scripts/setup-build-env.sh
@@ -15,21 +15,19 @@ export OBJCOPY=${PREFIX}objcopy
 export ELF2DOL=${DEVKITPPC}/bin/elf2dol
 
 export MACHDEP="-g -mcpu=750 -meabi -mhard-float -mrvl"
-export OPTIMS="" #-O3 -ffunction-sections -fdata-sections -fmodulo-sched"
+export OPTIMS="-O3 -ffunction-sections -fdata-sections -fmodulo-sched"
 export DEFS="-DGEKKO -DHW_RVL"
 export LIBS="-lfat -lwiiuse -lbte -logc -lm -lwiikeyboard"
 export CFLAGS="${MACHDEP} ${OPTIMS} ${DEFS}"
 export CXXFLAGS="${CFLAGS}"
 
 LIBOGC_INCLUDE_PATH="${DEVKITPRO}/libogc/include"
-export CPPFLAGS=" ${MACHDEP} \
- -I${LIBOGC_INCLUDE_PATH} \
+export CPPFLAGS="-I${LIBOGC_INCLUDE_PATH} \
  -I${PPC_INCLUDE_PATH} \
  -I${WII_INCLUDE_PATH}"
 
 LIBOGC_LIB_PATH="${DEVKITPRO}/libogc/lib/wii"
-export LDFLAGS="${MACHDEP} \
- -L${LIBOGC_LIB_PATH} \
+export LDFLAGS="-L${LIBOGC_LIB_PATH} \
  -L${PPC_LIB_PATH} \
  -L${WII_LIB_PATH}"
 
