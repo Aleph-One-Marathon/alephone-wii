d3e0ed2a50ecdc874c0d8af1cbbe2be98466be2e
Mathieu Deletrain <mathieudel@gmail.com>
2012-02-23 02:16:52 +0100

    Allow build arguments to traverse top-level scripts.

--- scripts/build-sdl.sh
+++ scripts/build-sdl.sh
@@ -13,20 +13,20 @@ source setup-build-env.sh
 
 export PKG_CONFIG=`which pkg-config`
 
-./build "${SUB_PROJECTS_PATH}/SDL" --prefix=${PORTLIBS_WII}
+./build "${SUB_PROJECTS_PATH}/SDL" $@ --prefix=${PORTLIBS_WII}
 
-./build "${SUB_PROJECTS_PATH}/SDL_image" --prefix=${PORTLIBS_WII} --with-sdl-prefix=${PORTLIBS_WII} \
+./build "${SUB_PROJECTS_PATH}/SDL_image" $@ --prefix=${PORTLIBS_WII} --with-sdl-prefix=${PORTLIBS_WII} \
     --disable-tif --disable-webp
 
-./build "${SUB_PROJECTS_PATH}/smpeg" --prefix=${PORTLIBS_WII} --with-sdl-prefix=${PORTLIBS_WII} \
+./build "${SUB_PROJECTS_PATH}/smpeg" $@ --prefix=${PORTLIBS_WII} --with-sdl-prefix=${PORTLIBS_WII} \
     --disable-debug --disable-gtk-player --disable-opengl-player
 
-./build "${SUB_PROJECTS_PATH}/SDL_mixer" --prefix=${PORTLIBS_WII} --with-sdl-prefix=${PORTLIBS_WII} --with-smpeg-prefix=${PORTLIBS_WII} \
+./build "${SUB_PROJECTS_PATH}/SDL_mixer" $@ --prefix=${PORTLIBS_WII} --with-sdl-prefix=${PORTLIBS_WII} --with-smpeg-prefix=${PORTLIBS_WII} \
     --enable-music-ogg --enable-music-ogg-tremor --disable-music-cmd --disable-music-mod --disable-music-fluidsynth-midi --disable-music-flac
 
-./build "${SUB_PROJECTS_PATH}/SDL_net" --prefix=${PORTLIBS_WII} --with-sdl-prefix=${PORTLIBS_WII} \
+./build "${SUB_PROJECTS_PATH}/SDL_net" $@ --prefix=${PORTLIBS_WII} --with-sdl-prefix=${PORTLIBS_WII} \
     --disable-gui
 
-./build "${SUB_PROJECTS_PATH}/SDL_ttf" --prefix=${PORTLIBS_WII} --with-sdl-prefix=${PORTLIBS_WII} --with-freetype-prefix=${PORTLIBS_PPC} \
+./build "${SUB_PROJECTS_PATH}/SDL_ttf" $@ --prefix=${PORTLIBS_WII} --with-sdl-prefix=${PORTLIBS_WII} --with-freetype-prefix=${PORTLIBS_PPC} \
     --without-x
 
--- scripts/configure-aleph-one.sh
+++ scripts/configure-aleph-one.sh
@@ -15,6 +15,6 @@ prefix=${TARGET_PATH}
 dataDir=${BIN_PATH}
 binDir=${BIN_PATH}/${APPS_DIR}/${ALEPH_DIR}
 
-./build "${SUB_PROJECTS_PATH}/aleph-one" -M=no --prefix=${prefix} --datadir=${dataDir} --bindir=${binDir} \
+./build "${SUB_PROJECTS_PATH}/aleph-one" -M=no $@ --prefix=${prefix} --datadir=${dataDir} --bindir=${binDir} \
 --with-sdl-prefix=${PORTLIBS_WII} --program-prefix="" --program-suffix=".elf" --program-transform-name="" \
 --disable-opengl --disable-speex --disable-zzip
