3fffc653cda66826f4b611c027c4388fe0156456
Mathieu Deletrain <mathieudel@gmail.com>
2012-09-22 03:43:28 +0200

    Add Wii's SDL m4 parent to aclocal's search path.

--- scripts/configure-aleph-one.sh
+++ scripts/configure-aleph-one.sh
@@ -18,6 +18,8 @@ prefix=${TARGET_PATH}
 dataDir=${BIN_PATH}
 binDir=${BIN_PATH}/${APPS_DIR}/${ALEPH_DIR}
 
+export ACLOCAL_FLAGS="-I ${PORTLIBS_WII}/share/aclocal"
+
 ./build "${SUB_PROJECTS_PATH}/aleph-one" -M=no $@ --prefix=${prefix} --datadir=${dataDir} --bindir=${binDir} \
 --with-sdl-prefix=${PORTLIBS_WII} --program-prefix="" --program-suffix=".elf" --program-transform-name="" \
 --disable-opengl --disable-speex --disable-zzip
