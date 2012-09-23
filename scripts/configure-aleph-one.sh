ca71aec84a8d5f72598aaac2f953f55199b76975
Mathieu Deletrain <mathieudel@gmail.com>
2012-09-23 02:43:34 +0200

    Finished dev machine migration by added missing bits to make build scripts usable under MacOs instead of Linux. Will take care of multiplateform later, if need be.

--- scripts/configure-aleph-one.sh
+++ scripts/configure-aleph-one.sh
@@ -3,6 +3,7 @@
 source setup-build-env.sh
 
 
+export ACLOCAL_FLAGS="-I ${PORTLIBS_WII}/share/aclocal -I /usr/local/share/aclocal/"
 export LIBS="${LIBS} -ljpeg -lpng -lfreetype -lvorbisidec -lz -lnetport"
 #export LDFLAGS="${LDFLAGS} -mcpu=750 -meabi -mhard-float -mrvl -msdata=eabi"
 export LDFLAGS="${LDFLAGS} -mcpu=750 -meabi -mhard-float -mrvl"
@@ -17,8 +18,6 @@ export VORBISFILE_LIBS=" "
 prefix=${TARGET_PATH}
 dataDir=${BIN_PATH}
 binDir=${BIN_PATH}/${APPS_DIR}/${ALEPH_DIR}
-
-export ACLOCAL_FLAGS="-I ${PORTLIBS_WII}/share/aclocal"
 
 ./build "${SUB_PROJECTS_PATH}/aleph-one" -M=no $@ --prefix=${prefix} --datadir=${dataDir} --bindir=${binDir} \
 --with-sdl-prefix=${PORTLIBS_WII} --program-prefix="" --program-suffix=".elf" --program-transform-name="" \
